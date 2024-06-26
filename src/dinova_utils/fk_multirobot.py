82324652da21a7a0b321dab676eeddcec97d1b8b
saraybakker1 <saraybakker@gmail.com>
2024-06-26 13:03:03 +0200

    two versions of objects, with fk and without for obstacle avoidance

--- src/dinova_utils/fk_multirobot.py
+++ src/dinova_utils/fk_multirobot.py
@@ -10,22 +10,17 @@ from geometry_msgs.msg import PoseStamped
 from derived_object_msgs.msg import Object, ObjectArray
 
 class FKMultiRobot():
-    # ---- Problem in this file: --- #
-    # should use q from the other robot, but is using q from the ego robot for defining the sphere positions ---#
     def __init__(self, robot_name):
-        # for now: define variables here ---------------------#
+        # ---- variables from yaml file ---- #
         self.robot_name = robot_name
-        self.ego_agent = self.robot_name
-        if self.ego_agent == "dingo2":
-            self.other_agents = ["dingo1"]
-        else:
-            self.other_agents = ["dingo2"]
-        self.collision_links = {self.other_agents[0]: ["chassis_link", "arm_tool_frame"]}
+        self.other_agents = rospy.get_param("all_agents")
+        self.other_agents.remove(self.robot_name)
+        self.collision_links = rospy.get_param("collision_links")
         # ---------------------------------------- #
         rospack = rospkg.RosPack()
         self._q_other_agents = [None] * len(self.other_agents)
         for other_agent in self.other_agents:
-            lidar_argument = rospy.get_param(other_agent+'/lidar') 
+            lidar_argument = rospy.get_param("/"+other_agent+'/lidar') 
             if lidar_argument == True:
                 agent_name = "dinova_lidar"
             else:
@@ -35,7 +30,8 @@ class FKMultiRobot():
         self._init_subscribers()
         
     def _init_subscribers(self):
-        self._joint_states_sub = rospy.Subscriber(self.other_agents[0]+'/dinova/omni_states_vicon', JointState, self._joint_states_cb)
+        # --- currently only subscribing to 1 other robot ---- #
+        self._joint_states_sub = rospy.Subscriber("/"+self.other_agents[0]+'/dinova/omni_states_vicon', JointState, self._joint_states_cb)
 
     def _joint_states_cb(self, msg: JointState):
         self._q_other_agents[0] = np.array(msg.position)[0:9]
@@ -52,8 +48,6 @@ class FKMultiRobot():
     def object_naming(self, object_names:List[str]) -> List[str]:
         for agent in self.other_agents:
             if agent in object_names:
-                print("agent:", agent)
-                print("object_names:", object_names)
                 object_names.remove(agent)
             for collision_link in self.collision_links[agent]:
                 object_names.append(agent+"_"+collision_link)
@@ -61,17 +55,11 @@ class FKMultiRobot():
                 
     def collision_spheres_other_agent(self, object_names, object_poses):
         object_poses_full = copy.deepcopy(object_poses)
-        print("object_poses:", object_poses)
         if self._q_other_agents[0] is not None:
-            object_poses_full.pop(self.ego_agent)
-            print("hereeee")
             for agent in self.other_agents:
-                print("hereeee2")
-                print("object_names:", object_names)
                 if agent in object_names:
                     object_poses_full.pop(agent)
                     for collision_link in self.collision_links[agent]:
-                        print("hereeee3")
                         object_pose = self.forward_kinematics.numpy(q=self._q_other_agents[0],
                                                     parent_link = "base_link",
                                                     child_link = collision_link,
@@ -82,6 +70,5 @@ class FKMultiRobot():
                         object_poses_full[object_name].pose.position.y = object_pose[1]
                         object_poses_full[object_name].pose.position.z = object_pose[2]
                         object_poses_full[object_name].header.frame_id = "map"
-        print("object_poses_full:", object_poses_full)
         return object_poses_full
     
