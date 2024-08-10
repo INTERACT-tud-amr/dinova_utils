d151022cad0a141308e2989375fe81c9899c559d
DanningZhao <danningzhao4021@gmail.com>
2024-08-10 14:44:38 +0200

    replace the test node with the fk modle

--- src/dinova_utils/fk_multirobot_pin.py
+++ src/dinova_utils/fk_multirobot_pin.py
@@ -1,44 +1,91 @@
+#!/usr/bin/env python
+import rospkg
 import rospy
-import numpy as np
-from sensor_msgs.msg import JointState
+#from forwardkinematics.urdfFks.generic_urdf_fk import GenericURDFFk
+#import pinocchio for fk
 from fk_pin import MobileManipulatorKinematics
 
-class DinovStateSubscriber:
-    def __init__(self):
-        # Initialize the ROS node
-        rospy.init_node('pinocchio_fk', anonymous=True)
+import numpy as np
+from sensor_msgs.msg import JointState
+from typing import Union, Dict, List
+import copy
+from geometry_msgs.msg import PoseStamped
+from derived_object_msgs.msg import Object, ObjectArray
 
-        self.agent_name = "dingo1"
-        self._robot_kinematics = MobileManipulatorKinematics()#initialize before the subscriber, othersie no arribute
+
+class FKMultiRobot():
+    def __init__(self, robot_name):
+        # ---- variables from yaml file ---- #
+        self.robot_name = robot_name
+        self.other_agents = rospy.get_param("all_agents")
         
-        self._joint_states_sub = rospy.Subscriber("/"+self.agent_name+'/dinova/omni_states_vicon', JointState, self._joint_states_cb)
-        self.joint_states = [None]
-
-
-    def _joint_states_cb(self, msg):
-        self.joint_states[0] = np.array(msg.position)[0:9]
-        #print(self.joint_states[0])
-        self._get_fk()
-        #print(msg.position[0:9])
-        #print(self.joint_states[0]) otherwise index error
-        #self._robot_kinematics#.forward(self.joint_states[0])
-    def _get_fk(self,link_name = None):
-        self._robot_kinematics.forward(self.joint_states[0])
-        print(self._robot_kinematics.link_pose()[0])
-        print(self._robot_kinematics.link_pose("chassis_link")[0])
+        del self.other_agents[self.robot_name]
+        #self.collision_links = self.other_agents[robot_name]['collision_links']
+        # ---------------------------------------- #
+        #rospack = rospkg.RosPack()
+        self._q_other_agents = [None] * len(self.other_agents)
         
+        # for other_agent in self.other_agents:
+        #     lidar_argument = self.other_agents[other_agent]['lidar']
+        #     if lidar_argument == True:
+        #         agent_name = "dinova_lidar"
+        #     else:
+        #         agent_name = "dinova"
+        #     URDF_FILE = rospack.get_path("dinova_fabrics_wrapper") + "/config/" + agent_name + ".urdf"
+        # self.symbolic_fk(URDF_FILE)
         
-    def spin(self):
-        # Keep the node alive and listening
-        rospy.spin()
+        self.forward_robot_kinematics = MobileManipulatorKinematics()#initialize before the subscriber, othersie no arribute
+        self._init_subscribers()
+        
+    def _init_subscribers(self):
+        # --- currently only subscribing to 1 other robot ---- #
+        other_agent_name = list(self.other_agents.keys())[0]
+        
+        #self._joint_states_sub = rospy.Subscriber("/"+other_agent_name+'/dinova/omni_states_vicon', JointState, self._joint_states_cb)
+        self._joint_states_sub = rospy.Subscriber("/"+other_agent_name+'/dinova/omni_states', JointState, self._joint_states_cb)
 
-if __name__ == '__main__':
-    try:
-        # # Instantiate the subscriber class
-        subscriber = DinovStateSubscriber()
-
-        # Start listening to the topic
-        subscriber.spin()
-
-    except rospy.ROSInterruptException:
-        pass
+    def _joint_states_cb(self, msg: JointState):
+        self._q_other_agents[0] = np.array(msg.position)[0:9]
+            
+    # def symbolic_fk(self, URDF_FILE) -> GenericURDFFk:
+    #     with open(URDF_FILE, "r", encoding="utf-8") as file:
+    #         urdf = file.read()
+    #     self.forward_kinematics = GenericURDFFk(
+    #         urdf,
+    #         root_link="base_link",
+    #         end_links=["arm_tool_frame", "arm_orientation_helper_link"],
+    #     )
+        
+    def object_naming(self, object_names:List[str]) -> List[str]:#maybe not used???
+        for agent in self.other_agents:
+            if agent in object_names:
+                object_names.remove(agent)
+            for collision_link in self.collision_links[agent]:
+                object_names.append(agent+"_"+collision_link)
+        return object_names
+                
+    def collision_spheres_other_agent(self, object_names, object_poses):
+        object_poses_full = copy.deepcopy(object_poses)
+        if self._q_other_agents[0] is not None:
+            for agent_name, agent in self.other_agents.items():
+                if agent_name in object_names:#defined in vicon_dinova.yaml
+                    object_poses_full.pop(agent_name)#pop the agent's correpsonding pose and attach its fk poses
+                    #Apply forward kinematics to the robot by configuration
+                    self.forward_robot_kinematics.forward(q=self._q_other_agents[0])
+                    for collision_link in agent['collision_links']:# list
+                        
+                        # object_pose = self.forward_kinematics.numpy(q=self._q_other_agents[0],
+                        #                             parent_link = "base_link",
+                        #                             child_link = collision_link,
+                        #                             position_only=True)
+                        # replaced by pinocchio
+                        object_pose = self.forward_robot_kinematics.link_pose("chassis_link")[0]
+                        
+                        object_name = agent_name+"_"+collision_link
+                        object_poses_full[object_name] = PoseStamped()
+                        object_poses_full[object_name].pose.position.x = object_pose[0]
+                        object_poses_full[object_name].pose.position.y = object_pose[1]
+                        object_poses_full[object_name].pose.position.z = object_pose[2]
+                        object_poses_full[object_name].header.frame_id = "map"
+        return object_poses_full
+    
