1771e4d9dda48ceebc088bf4b5e5b93482651c05
DanningZhao <danningzhao4021@gmail.com>
2024-08-30 12:10:05 +0200

    add twist for objects

--- src/dinova_utils/fk_multirobot_pin.py
+++ src/dinova_utils/fk_multirobot_pin.py
@@ -10,6 +10,7 @@ from sensor_msgs.msg import JointState
 from typing import Union, Dict, List
 import copy
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
 from derived_object_msgs.msg import Object, ObjectArray
 
 
@@ -59,13 +60,14 @@ class FKMultiRobot():
                 
     def collision_spheres_other_agent(self, object_names, object_poses):
         object_poses_full = copy.deepcopy(object_poses)
+        object_twists = {} #a dictionary to hold twists 
         if self._q_other_agents[0] is not None:
             for agent_name, agent in self.other_agents.items():
                 if agent_name in object_names and agent_name in object_poses:
                     # 1. agent_name is defined in vicon_dinova.yaml 
                     # 2. only pop it when it is available, otherwise error!
                     object_poses_full.pop(agent_name)#pop the agent's correpsonding pose and attach its fk poses
-                    #Apply forward kinematics to the robot by configuration
+                    #Apply forward kinematics to the robot 
                     self.forward_robot_kinematics.forward(q=self._q_other_agents[0], v=self._v_other_agents[0])
                     for collision_link in agent['collision_links']:# list
                         # object_pose = self.forward_kinematics.numpy(q=self._q_other_agents[0],
@@ -73,7 +75,9 @@ class FKMultiRobot():
                         #                             child_link = collision_link,
                         #                             position_only=True)
                         # replaced by pinocchio
+                        # only get the translation part
                         object_pose = self.forward_robot_kinematics.link_pose(collision_link)[0]
+                        object_twist= self.forward_robot_kinematics.link_velocity(collision_link)[0]
                         
                         object_name = agent_name+"_"+collision_link
                         object_poses_full[object_name] = PoseStamped()
@@ -81,5 +85,11 @@ class FKMultiRobot():
                         object_poses_full[object_name].pose.position.y = object_pose[1]
                         object_poses_full[object_name].pose.position.z = object_pose[2]
                         object_poses_full[object_name].header.frame_id = "map"
-        return object_poses_full
+                        
+                        object_twists[object_name] = Twist()
+                        object_twists[object_name].linear.x = object_twist[0]
+                        object_twists[object_name].linear.y = object_twist[1]
+                        object_twists[object_name].linear.z = object_twist[2]
+                        
+        return object_poses_full, object_twists
     
